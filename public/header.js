8912a240ebe32687d4ec8a03d3a32841cdcfdf72
Serena Leung <serena@Serenas-MacBook-Pro.local>
2017-08-04 09:39:06 -0700

    Fixed site to support higher resolution and other styling

--- public/header.js
+++ public/header.js
@@ -1,34 +1,15 @@
-var cbp<a href="http://www.jqueryscript.net/tags.php?/Animated/">Animated</a>Header = (function() {
-
-    var docElem = document.documentElement,
-        header = document.querySelector( '.cbp-af-header' ),
-        did<a href="http://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a> = false,
-        changeHeaderOn = 300;
-
-    function init() {
-        window.addEventListener( 'scroll', function( event ) {
-            if( !didScroll ) {
-                didScroll = true;
-                setTimeout( scrollPage, 250 );
-            }
-        }, false );
-    }
-
-    function scrollPage() {
-        var sy = scrollY();
-        if ( sy >= changeHeaderOn ) {
-            classie.add( header, 'navbar-shrink' );
-        }
-        else {
-            classie.remove( header, 'navbar-shrink' );
-        }
-        didScroll = false;
-    }
-
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
-    }
-
-    init();
-
-})();
+$(function(){
+var shrinkHeader = 300;
+$(window).scroll(function() {
+  var scroll = getCurrentScroll();
+    if ( scroll >= shrinkHeader ) {
+         $('.navbar-default').addClass('shrink');
+      }
+      else {
+          $('.navbar-default').removeClass('shrink');
+      }
+});
+function getCurrentScroll() {
+  return window.pageYOffset || document.documentElement.scrollTop;
+  }
+});
